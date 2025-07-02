a8991d40cb75e20dba484921e27c047b62ae8902
Thamid Zaman <thamidzaman@Thamids-MacBook-Air.local>
2025-07-02 15:40:07 -0700

    implement login_page code

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,9 +4,8 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.username_field = (By.ID, "username")
-        self.password_field = (By.ID, "password")
-        self.continue_username_button = (By.CLASS_NAME, "ccbf5f810 c0a334fa0 c8f34b77b cec7560fc_button-login-id")
-        self.continue_password_button = (By.CLASS_NAME, "ccbf5f810 c0a334fa0 c8f34b77b cec7560fc_button-login-password")
+        self.create_account_button = (By.XPATH, "//a[@class='c15c36395 c0e6110c2']")
+        self.continue_username_button = (By.XPATH, "//button[@class='ccbf5f810 c0a334fa0 c8f34b77b cec7560fc _button-login-id']")
         self.login_button = (By.ID, "login-button")
         
     def open(self, url):
@@ -14,13 +13,10 @@ class LoginPage:
 
     def enter_username(self, username):
         self.driver.find_element(*self.username_field).send_keys(username)
-
-    def enter_password(self, password):
-        self.driver.find_element(*self.password_field).send_keys(password)
     
     def click_continue_username(self):
         self.driver.find_element(*self.continue_username_button).click()
-    
-    def click_continue_password(self):
-        self.driver.find_element(*self.continue_password_button).click()
+
+    def click_create_account_button(self):
+        self.driver.find_element(*self.create_account_button).click()
     
