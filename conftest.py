7301c74a151a3821cde3e35fc9f22e64cf9af7a8
Thamid Zaman <thamidzaman@Thamids-MacBook-Air.local>
2025-07-03 12:04:31 -0700

    update .gitignore and conftest files

--- conftest.py
+++ conftest.py
@@ -2,11 +2,27 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+'''
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # Attach screenshot to Allure on failure
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("browser")
+        if driver:
+            try:
+                allure.attach(driver.get_screenshot_as_png(), name="screenshot", attachment_type=allure.attachment_type.PNG)
+            except Exception as e:
+                pass
+'''
+
 @pytest.fixture
 def browser():
     options = Options()
     #options.add_argument("--headless=False")  # Run in headless mode
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)  # Wait up to 10 seconds for elements
+    driver.maximize_window()
     yield driver
     driver.quit()
